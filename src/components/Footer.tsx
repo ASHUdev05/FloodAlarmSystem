dca8e5e35038f6e42aaa8cdfff983090586ece4e
ASHUdev05 <amphoteric629@gmail.com>
2025-11-07 14:02:33 +0530

    Fixed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-blue-900 text-white text-center py-3 mt-8">
-      <p>© 2025 Flood Alarm System | Designed by UEM</p>
+      <p>© 2025 Flood Alarm System | Designed with ❤️ in 🇮🇳 </p>
     </footer>
   );
 }
